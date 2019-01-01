fc9de820e6a782a9549a5b6207123785242f6b0f
Carlsilber <carl.silber74@gmail.com>
2019-01-01 18:17:23 +0200

    adding POJOs. Closes #2

---
+++ src/main/java/com/carlsilber/petclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.carlsilber.petclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/carlsilber/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.carlsilber.petclinic.model;
+
+public class Vet extends Person {
+
+}
