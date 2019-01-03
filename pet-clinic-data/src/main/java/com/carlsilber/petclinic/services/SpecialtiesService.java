0f72a812cfd2ded2488bcf16f6a1db4c7f3d26c1
Carlsilber <carl.silber74@gmail.com>
2019-01-03 16:37:45 +0200

    Added Specialties Map Implementation. Closes #31

---
+++ pet-clinic-data/src/main/java/com/carlsilber/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.carlsilber.petclinic.services;
+
+import com.carlsilber.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/carlsilber/petclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,34 @@
+package com.carlsilber.petclinic.services.map;
+
+import com.carlsilber.petclinic.model.Specialty;
+import com.carlsilber.petclinic.services.SpecialtiesService;
+import java.util.Set;
+
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements
+    SpecialtiesService {
+
+  @Override
+  public Set<Specialty> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public Specialty findById(Long id) {
+    return super.findById(id);
+  }
+
+  @Override
+  public Specialty save(Specialty object) {
+    return super.save(object);
+  }
+
+  @Override
+  public void delete(Specialty object) {
+    super.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+}
