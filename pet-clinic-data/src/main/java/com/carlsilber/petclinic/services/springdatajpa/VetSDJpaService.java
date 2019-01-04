76a1de73f4df64dcddae7eb7ee2f37d6a8ec6d09
Carlsilber <carl.silber74@gmail.com>
2019-01-04 21:17:58 +0200

    added vet spring data jpa. Closes #41

---
+++ pet-clinic-data/src/main/java/com/carlsilber/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,47 @@
+package com.carlsilber.petclinic.services.springdatajpa;
+
+import com.carlsilber.petclinic.model.Vet;
+import com.carlsilber.petclinic.repositories.VetRepository;
+import com.carlsilber.petclinic.services.VetService;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+  private final VetRepository vetRepository;
+
+  public VetSDJpaService(VetRepository vetRepository) {
+    this.vetRepository = vetRepository;
+  }
+
+  @Override
+  public Set<Vet> findAll() {
+    Set<Vet> vets = new HashSet<>();
+    vetRepository.findAll().forEach(vets::add);
+    return vets;
+  }
+
+  @Override
+  public Vet findById(Long aLong) {
+    return vetRepository.findById(aLong).orElse(null);
+  }
+
+  @Override
+  public Vet save(Vet object) {
+    return vetRepository.save(object);
+  }
+
+  @Override
+  public void delete(Vet object) {
+    vetRepository.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long aLong) {
+    vetRepository.deleteById(aLong);
+  }
+}
