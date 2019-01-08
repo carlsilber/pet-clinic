ae0de719cebe9dada7f3f9481805518bc3875bbb
Carlsilber <carl.silber74@gmail.com>
2019-01-08 23:21:19 +0200

    added Project Lombok to project. Closes #51

--- pet-clinic-data/src/main/java/com/carlsilber/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/carlsilber/petclinic/model/Visit.java
@@ -6,7 +6,17 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
